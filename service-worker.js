42b010a6ecb04ae9aec31cf5d416731d884b6fa6
Semigul <madeleine.marmborg@gmail.com>
2026-07-14 14:19:57 +0200

    Lägg till illustrerade rollkort och nya regler

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'blush-bluff-v2';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'blush-bluff-v3';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './assets/roles/brow-stylist.png', './assets/roles/drama-queen.png', './assets/roles/gloss-mute.png', './assets/roles/lookalike-artist.png', './assets/roles/precision-liner.png', './assets/roles/shade-reader.png', './assets/roles/smudge-wolf.png', './assets/roles/topcoat-guardian.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
